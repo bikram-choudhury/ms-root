8da8f13b8cd22298dfb3ec2d3c1a4a103af95da3
Bikram Choudhury <bikram.choudhury527@gmail.com>
2019-05-16 21:38:50 +0530

    Holding the serverside rendering for now because css is not loading from server side. But basics of serverside rendering is done

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import AppComponent from './components/App.component.jsx';
 
 // ReactDOM.render(<h1>This is my First React App</h1>, document.getElementById('root'));
-// ReactDOM.render(<AppComponent />, document.getElementById('root'));
+ReactDOM.render(<AppComponent />, document.getElementById('root'));
 
-ReactDOM.render(React.createElement(AppComponent), document);
+// ReactDOM.render(React.createElement(AppComponent), document);
